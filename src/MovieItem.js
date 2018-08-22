8960dc8c6fa00b2a05624a5dff5de1d10c425dde
Arina Pricop <arina.carmen@yahoo.com>
2018-08-22 20:24:40 +0300

    movie cards

---
+++ src/MovieItem.js
@@ -0,0 +1,20 @@
+import React from "react";
+import Card from "@material-ui/core/Card";
+import CardContent from "@material-ui/core/CardContent";
+import Typography from "@material-ui/core/Typography";
+
+const MovieItem = props => {
+  return (
+    <Card className="movie-item">
+      <CardContent>
+        <Typography variant="headline" component="h3">
+          {props.title}
+        </Typography>
+        <Typography color="textSecondary">{props.year}</Typography>
+        <Typography component="p">{props.description}</Typography>
+      </CardContent>
+    </Card>
+  );
+};
+
+export default MovieItem;
--- src/MovieList.js
+++ src/MovieList.js
@@ -1,6 +1,8 @@
 import React from "react";
 import Button from "@material-ui/core/Button";
 import { InputLabel } from "../node_modules/@material-ui/core";
+import MovieItem from "./MovieItem";
+import styles from "./MovieList.css";
 
 function MovieList() {
   return (
@@ -11,6 +13,17 @@ function MovieList() {
       />
       <Button className="search-btn" variant="contained" color="secondary">
         Search movie
+      </Button>
+
+      <div className="movies-container">
+        <MovieItem
+          title="Indepedence Day"
+          year="1996"
+          description="Fancy Description"
+        />
+      </div>
+      <Button className="search-btn" variant="contained" color="primary">
+        Add Movie
       </Button>
     </div>
   );
